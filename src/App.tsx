0b8ff3fef5fec355fb9ab58a7e15b597810d3b59
Yousria Debaud <ydebaud@meilleursagents.com>
2022-05-03 16:44:34 +0200

    fix(vault): apply eslint and prettier fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,62 @@
-import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
-import Input from './Input'
+import Input from './Input';
 
-const API = 'http://localhost:5000'
+const API = 'http://localhost:5000';
 
 type Notif = {
-  id: string,
-  type: string,
-  // FIXME we should *probably* not have this `any`
-  data: any
-}
+    id: string;
+    type: string;
+    // FIXME we should *probably* not have this `any`
+    data: any;
+};
 
 const App = () => {
-  const [searchText, setSearchText] = useState('')
-  const [isLoading, setLoading] = useState(false)
-  const [results, setResults] = useState<null | Notif[]>(null)
+    const [searchText, setSearchText] = useState('');
+    const [isLoading, setLoading] = useState(false);
+    const [results, setResults] = useState<null | Notif[]>(null);
 
-  useEffect(() => {
-    const effect = async () => {
-      // FIXME there is something wrong with this loading state... to be investigated :D
-      setLoading(true)
-      const res = await fetch(`${API}/search?q=${searchText}`)
-      const data = await res.json()
-      setResults(data)
-    }
-    effect();
-  }, [searchText, setLoading, setResults])
+    useEffect(() => {
+        const effect = async () => {
+            // FIXME there is something wrong with this loading state... to be investigated :D
+            setLoading(true);
+            const res = await fetch(`${API}/search?q=${searchText}`);
+            const data = await res.json();
+            setResults(data);
+        };
+        effect();
+    }, [searchText, setLoading, setResults]);
 
-  return (
-    <Container>
-    <Input value={searchText} onChange={setSearchText}
-      placeholder="Type to filter events"
-    />
-    {isLoading ? <div>{"Loading..."}</div> : results ? (
-      <div>
-      {results.map(
-        r => (
-          // TODO we must finalize this integration!! not very pretty like this
-          <Item>{JSON.stringify(r)}</Item>
-        )
-      )}
-      </div>
-    ) : null}
-    </Container>
-  )
-}
+    return (
+        <Container>
+            <Input
+                value={searchText}
+                onChange={setSearchText}
+                placeholder="Type to filter events"
+            />
+            {isLoading ? (
+                <div>{'Loading...'}</div>
+            ) : results ? (
+                <div>
+                    {results.map((r) => (
+                        // TODO we must finalize this integration!! not very pretty like this
+                        <Item>{JSON.stringify(r)}</Item>
+                    ))}
+                </div>
+            ) : null}
+        </Container>
+    );
+};
 
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+`;
 
 const Item = styled.div`
-  border: 2px dashed red;
-`
+    border: 2px dashed red;
+`;
 
-export default App
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import { render } from 'react-dom'
+import React from 'react';
+import { render } from 'react-dom';
 
-import App from './App'
-import GlobalStyle from './GlobalStyle'
+import App from './App';
+import GlobalStyle from './GlobalStyle';
 
-const rootNode = document.getElementById('root')
+const rootNode = document.getElementById('root');
 
-render(<>
-       <GlobalStyle />
-       <App />
-</>, rootNode)
+render(
+    <>
+        <GlobalStyle />
+        <App />
+    </>,
+    rootNode,
+);
