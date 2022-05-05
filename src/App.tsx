a5cd45e3d63102ad07dae6c7e85b20678dd1c546
Yousria <yousria.benchadi@gmail.com>
2022-05-05 22:03:15 +0200

    feat(vault): handle error and 0 results

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,24 @@ import Spinner from 'components/Spinner';
 import type { Notif } from 'types/Notif';
 import NotifItem from 'components/NotifItem';
 import useSWR from 'swr';
+import noResultsImage from 'url:images/noResults.png';
+import errorImage from 'url:images/errorImage.png';
+import { Typography } from '@mui/material';
 
 const App = () => {
     const [searchText, setSearchText] = useState('');
 
-    const { data: results, error } = useSWR<Notif[]>(`/search?q=${searchText}`);
+    const { data: results, error, isValidating } = useSWR<Notif[]>(`/search?q=${searchText}`);
+    const noResults = !isValidating && results?.length === 0;
+
+    if (error) {
+        return (
+            <StyledDiv>
+                <StyledImg height={300} width={350} src={errorImage} alt="An error happened ..!" />
+                <Typography>{`An error happened ..!`}</Typography>
+            </StyledDiv>
+        );
+    }
 
     return (
         <Container>
@@ -23,6 +36,17 @@ const App = () => {
                 <Spinner />
             ) : results ? (
                 <Container>
+                    {noResults && (
+                        <StyledDiv>
+                            <StyledImg
+                                height={300}
+                                width={350}
+                                src={noResultsImage}
+                                alt="No results found..."
+                            />
+                            <Typography>{`No results found...`}</Typography>
+                        </StyledDiv>
+                    )}
                     {results.map((notification) => (
                         <NotifItem key={notification.id} notification={notification} />
                     ))}
@@ -39,6 +63,19 @@ const Container = styled.div`
     align-items: flex-start;
     height: 100%;
     width: 100%;
+`;
+
+const StyledDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    align-self: center;
+`;
+
+const StyledImg = styled.img`
+    margin-bottom: 1rem;
+    margin-top: 15rem;
 `;
 
 export default App;
--- src/constants.ts
+++ src/constants.ts
@@ -3,6 +3,9 @@
  */
 export const API = 'http://localhost:5000';
 
+/**
+ * event types enum
+ */
 export enum EVENT_TYPES {
     TRANSACTION_RECEIVED = 'TRANSACTION_RECEIVED',
     TRANSACTION_SENT = 'TRANSACTION_SENT',
