f459e40f7d83d6f7ac6799ac4b421e8d46aa8066
Hadrien Rudich <hadrien.rudich@gmail.com>
2024-01-31 12:30:59 +0100

    fixed glaring logic flaws in app.py & App.tsx - searches are now functional

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import TextInput from "./components/TextInput";
 
 const API = "http://localhost:5000";
 
-type Notif = {
+type SearchResult = {
   id: string;
   type: string;
-  // FIXME we should *probably* not have this `any`
-  data: any;
+  data: {
+    id: number;
+    amount: number;
+    unit: string;
+    from: string;
+    to: string;
+  };
 };
 
 const App = () => {
   const [searchText, setSearchText] = useState("");
   const [isLoading, setLoading] = useState(false);
-  const [results, setResults] = useState<null | Notif[]>(null);
+  const [results, setResults] = useState<null | SearchResult[]>(null);
 
   useEffect(() => {
     const effect = async () => {
@@ -22,20 +27,27 @@ const App = () => {
       const res = await fetch(`${API}/search?q=${searchText}`);
       const data = await res.json();
       setResults(data);
+      setLoading(false);
     };
     effect();
   }, [searchText, setLoading, setResults]);
 
   return (
     <div>
-      <TextInput value={searchText} onChange={setSearchText} placeholder="Type to filter events" />
+      <TextInput
+        value={searchText}
+        onChange={setSearchText}
+        placeholder="Type to filter events"
+      />
       {isLoading ? (
         <div>{"Loading..."}</div>
       ) : results ? (
         <div>
           {results.map((r) => (
             // TODO we must finalize this integration!! not very pretty like this
-            <div className="border border-dashed">{JSON.stringify(r)}</div>
+            <div key={r.id} className="border border-dashed">
+              {JSON.stringify(r)}
+            </div>
           ))}
         </div>
       ) : null}
